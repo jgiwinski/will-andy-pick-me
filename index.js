f07aedac6b995a67b98c467ff359d847d0175801
jgiwinski <juliaiwinski@gmail.com>
2021-01-04 18:44:47 -0700

    Clear functionality

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var questionInput = document.querySelector('.input');
 var answerBtn = document.querySelector('.answer');
-var clearBrn = document.querySelector('.clear');
+var clearBtn = document.querySelector('.clear');
 var question = document.querySelector('.question');
 var response = document.querySelector('.response');
 var qAndA = document.querySelector('.eightball-decision');
 var eightball = document.querySelector('.eightball');
 
 answerBtn.addEventListener('click', displayResponse);
+clearBtn.addEventListener('click', clearAll);
 
 function randomResponse(array) {
   return Math.floor(Math.random() * array.length);
@@ -16,13 +17,28 @@ function displayResponse() {
   var newResponse = ballResponses[randomResponse(ballResponses)];
   var newInput = questionInput.value;
 
+  if(newInput === ""){
+    alert("uh oh, looks like you need to put in a question.")
+    reloadPage(); 
+  }
+
   qAndA.classList.remove('hidden');
   eightball.classList.add('hidden');
 
   question.innerText = newInput;
   response.innerText = newResponse;
+  clearInput()
+}
+
+function reloadPage() {
+  location.reload();
 }
 
 function clearInput() {
   questionInput.value = '';
+}
+
+function clearAll() {
+  qAndA.classList.add('hidden');
+  eightball.classList.remove('hidden');
 }
